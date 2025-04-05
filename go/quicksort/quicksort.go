199e387037b3b5981710e068ccdf209a3e21db30
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-05 20:23:19 -0300

    feat: added randomized quicksort

--- go/quicksort/quicksort.go
+++ go/quicksort/quicksort.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"errors"
 	"log"
+	"math/rand"
 )
 
 func partition[T cmp.Ordered](A []T, p int, r int) (int, error) {
@@ -26,6 +27,13 @@ func partition[T cmp.Ordered](A []T, p int, r int) (int, error) {
 	return i + 1, nil
 }
 
+func randomPartition[T cmp.Ordered](A []T, p int, r int) (int, error) {
+	i :=  p + rand.Intn(r)
+	A[i], A[r] = A[r], A[i]
+
+	return partition(A, p, r)
+}
+
 func Quicksort[T cmp.Ordered](A []T, p int, r int) {
 	if p >= len(A) || r >= len(A) {
 		log.Fatal("index out of range")
@@ -35,6 +43,22 @@ func Quicksort[T cmp.Ordered](A []T, p int, r int) {
 		if q, err := partition(A, p, r); err == nil {
 			Quicksort(A, p, q - 1)
 			Quicksort(A, q + 1, r)
+		} else {
+			log.Fatal(err.Error())
+		}
+	}
+}
+
+
+func RandomQuicksort[T cmp.Ordered](A []T, p int, r int) {
+	if p >= len(A) || r >= len(A) {
+		log.Fatal("index out of range")
+	}
+
+	if p < r {
+		if q, err := randomPartition(A, p, r); err == nil {
+			RandomQuicksort(A, p, q - 1)
+			RandomQuicksort(A, q + 1, r)
 		} else {
 			log.Fatal(err.Error())
 		}
