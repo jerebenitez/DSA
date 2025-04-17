c49abb82f67091186299801ecdfbc57752518ee2
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-17 12:59:45 -0300

    feat: added quicksort fuzz testing

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module github.com/jerebenitez/DSA/go
 
 go 1.21.4
+
+require github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6 // indirect
---
+++ go/quicksort/quicksort_test.go
@@ -0,0 +1,33 @@
+package quicksort
+
+import (
+	"sort"
+	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+)
+
+func FuzzQuicksort(f *testing.F) {
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fz := fuzz.NewConsumer(data)
+        var targetSlice []int
+        if err := fz.CreateSlice(&targetSlice); err != nil {
+			t.Skip()
+		}
+
+		oldLen := len(targetSlice)
+		t.Log(targetSlice, oldLen)
+		Quicksort(targetSlice, 0, oldLen - 1)
+
+		// check if sorted
+		if !sort.IntsAreSorted(targetSlice) {
+			t.Errorf("not sorted: %v", targetSlice)
+		}
+
+		// check for length consistency
+		if len(targetSlice) != oldLen {
+			t.Errorf("length changed: before %d, after %d", oldLen, len(targetSlice))
+		}
+	})
+}
