81d71ccfb595321ca63f002592cdde53c169bca9
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-04-17 13:03:18 -0300

    feat: fuzzing testing for RandomQuicksort

--- go/quicksort/quicksort_test.go
+++ go/quicksort/quicksort_test.go
@@ -31,3 +31,28 @@ func FuzzQuicksort(f *testing.F) {
 		}
 	})
 }
+
+func FuzzRandomQuicksort(f *testing.F) {
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fz := fuzz.NewConsumer(data)
+        var targetSlice []int
+        if err := fz.CreateSlice(&targetSlice); err != nil {
+			t.Skip()
+		}
+
+		oldLen := len(targetSlice)
+		t.Log(targetSlice, oldLen)
+		RandomQuicksort(targetSlice, 0, oldLen - 1)
+
+		// check if sorted
+		if !sort.IntsAreSorted(targetSlice) {
+			t.Errorf("not sorted: %v", targetSlice)
+		}
+
+		// check for length consistency
+		if len(targetSlice) != oldLen {
+			t.Errorf("length changed: before %d, after %d", oldLen, len(targetSlice))
+		}
+	})
+}
